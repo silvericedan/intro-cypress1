2180fd396f66b6dd612299ca8af3be502b92a70e
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-07-29 15:19:33 -0300

    accessing elements and interacting with them

--- cypress/integration/todomvc.spec.js
+++ cypress/integration/todomvc.spec.js
@@ -1,1 +1,16 @@
 /// <reference types="cypress" />
+
+it('should navigate to the TodoMVC App', () => {
+    //cy.visit('http://todomvc-app-for-testing.surge.sh')
+
+    //cy.get('.new-todo').type("Clean room{enter}")
+    //if a test takes more than 4 seconds, it fails. There is a workaround, see next
+
+    cy.visit('http://todomvc-app-for-testing.surge.sh/?delay-new-todo=5000')
+
+    cy.get('.new-todo', {timeout: 6000}).type("Clean room{enter}")
+
+    cy.get('.toggle').click()
+
+    cy.contains('Clear completed').click()
+})
