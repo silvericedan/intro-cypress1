2fb73d5083805d65a1f568020f4b19432ffed83c
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-08-03 16:05:24 -0300

    replaced all test calls with page objects

--- cypress/integration/todomvc-withpageobjects.spec.js
+++ cypress/integration/todomvc-withpageobjects.spec.js
@@ -16,22 +16,24 @@ describe('todo actions', () => {
 
         todoPage.validateTodoTxt(0, 'Clean room')
 
-        cy.get('.toogle').should('not.be.checked')
+        todoPage.validateToggleState(0,false)
     })
 
     it('should mark a todo as completed', () => {
 
-        cy.get('.toggle').click()
-        cy.get('label').should('have.css', 'text-decoration-line', 'line-through') //check to see if has the css of line-through
+        todoPage.toggleTodo(0)
+
+        todoPage.validateTodoCompletedState(0,true) //check to see if has the css of line-through
     })
 
     it('should clear completed todos', () => {
 
-        cy.get('.toggle').click()
+        todoPage.toggleTodo(0)
 
-        cy.contains('Clear').click()
+        todoPage.clearCompleted()
         
-        cy.get('.todo-list').should('not.have.descendants', 'li') //checking to see if the list have elements after clearing it
+        todoPage.validateListHaveElements(false)
+        //cy.get('.todo-list').should('not.have.descendants', 'li') //checking to see if the list have elements after clearing it
 
     })
 })
--- cypress/page-objects/todo-page.js
+++ cypress/page-objects/todo-page.js
@@ -10,4 +10,26 @@ export class TodoPage {
     validateTodoTxt(todoIndex, expectedText){
         cy.get(`.todo-list li:nth-child(${todoIndex +1}) label`).should('have.text', expectedText)
     }
+
+    toggleTodo(todoIndex){
+        cy.get(`.todo-list li:nth-child(${todoIndex +1}) .toggle`).click()
+    }
+
+    clearCompleted(){
+        cy.contains('Clear completed').click()
+    }
+
+    validateToggleState(todoIndex, shouldBeToggled){
+        const label = cy.get(`.todo-list li:nth-child(${todoIndex +1}) label`)
+        label.should(`${shouldBeToggled ? '' : 'not.'}be.checked`)
+    }
+
+    validateTodoCompletedState(todoIndex, shouldBeCompleted){
+        const l = cy.get(`.todo-list li:nth-child(${todoIndex +1}) label`)
+        l.should(`${shouldBeCompleted ? '' : 'not.'}have.css`, 'text-decoration-line', 'line-through')
+    }
+
+    validateListHaveElements(shouldHaveElements){
+        cy.get('.todo-list').should(`${shouldHaveElements? '' : 'not.'}have.descendants`, 'li')
+    }
 }
