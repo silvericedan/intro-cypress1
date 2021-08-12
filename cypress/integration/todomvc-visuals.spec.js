02246b9a9394955551712635c081aa6266dc5a27
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-08-12 17:20:06 -0300

    added multiple browser and resolution on visual testing

--- cypress/integration/todomvc-visuals.spec.js
+++ cypress/integration/todomvc-visuals.spec.js
@@ -2,10 +2,18 @@
 import * as todoPage from '../page-objects/todo-page-functions'
 
 describe('visual validation', () => {
-    //before(() => todoPage.navigate())
-    before(() => cy.visit('http://todomvc-app-for-testing.surge.sh/?different-title-color'))
+    before(() => todoPage.navigate())
+    //before(() => cy.visit('http://todomvc-app-for-testing.surge.sh/?different-title-color'))
 
-    beforeEach(() => cy.eyesOpen({appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!'}))
+    beforeEach(() => cy.eyesOpen({
+        appName: 'TAU TodoMVC', batchName: 'TAU TodoMVC Hey!',
+        browser: [
+            {name: 'chrome', width: 1024, height: 768},
+            {name: 'chrome', width: 800, height: 600},
+            {name: 'firefox', width: 1024, height: 768},
+            {deviceName: 'iPhone X'}
+        ]
+    }))
     afterEach(() => cy.eyesClose())
 
     it('should look good', () => {
